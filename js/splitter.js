e0dc979781c776bd4f2e5056fe0603cb5cbe96ab
David Hawkins <uxhawk@gmail.com>
2020-02-23 22:20:08 -0500

    modal window displays details from player card

--- js/splitter.js
+++ js/splitter.js
@@ -1,26 +1,15 @@
-// var checkInputField = document.getElementById("inputCheck");
-// var tipInputField = document.getElementById("tipPercentInput");
-// var finalBill, addedTip, checkAmount, tipPercent;
-
-// var calcTipBtn = document.getElementById("calc-tip-btn");
-
-
-
-
-
 var payerNameEl = document.getElementById("payerName");
 var payerContributionEl = document.getElementById("payerContribution");
 var bodyEl = document.getElementsByTagName("body")[0];
 var payerName, payerContribution;
 var numPayers = 0;
 var payerList = new Map();
-
 var addPayerBtn = document.getElementById("add-payer-btn");
 
 
 //event listeners
 addPayerBtn.addEventListener("click", addPayer);
-
+document.getElementById("save-payer-edits").addEventListener("click", editValues);
 
 //add event listeners to all input fields
 let inputFields = document.getElementsByClassName("form-control");
@@ -33,16 +22,24 @@ function setValues(element) {
     element.target.id === "payerName" ? payerName = payerNameEl.value : payerContribution = parseInt(payerContributionEl.value);
 }
 
+function editValues() {
+    document.getElementById("editPayerName").v
+}
+
 function addPayer(event) {
     event.preventDefault();
-    let payer = new BillPayer(payerName, payerContribution, 0);
-
-    payerList.set(numPayers, payer);
-    console.log(payerList);
-    displayPayer();
-    clearInputs();
-    numPayers++;
-
+    if (payerNameEl.value === "") {
+        alert("Please enter a name.");
+    } else if (payerContributionEl.value === "") {
+        alert("Please enter the amount this person spent.");
+    } else {
+        let payer = new BillPayer(payerName, payerContribution, 0);
+        payerList.set(numPayers, payer);
+        //console.log(payerList);
+        showPlayerCard();
+        clearInputs();
+        numPayers++;
+    }
 }
 
 function clearInputs() {
@@ -50,28 +47,40 @@ function clearInputs() {
     payerNameEl.value = "";
 }
 
-var cardBlock = `<div class="mt-5 col-md-4 payer-card"><div class="card bg-light mb-3" style="max-width: 18rem;"> <div class="card-header"></div> <div class="card-body"> <p class="card-title">Light card title</p><h5 class="card-text"></h5></div></div></div>`;
-
-
-function displayPayer() {
-
-    var curPayer = payerList.get(numPayers);
-    var curName = curPayer.name;
+function showPlayerCard() {
+    var cardBlock = `<div id="card-${numPayers}" class="mt-5 col-md-4 payer-card"><div class="card bg-light mb-3" style="max-width: 18rem;"> <div class="card-header card-header d-flex justify-content-between align-items-center">  </div> <div class="card-body"> <p class="card-title">Light card title</p><h5 class="card-text"></h5></div></div></div>`;
     document.body.insertAdjacentHTML("beforeend", cardBlock);
-    var curCard = document.body.lastElementChild;
-    console.log(curCard);
 
-    curCard.getElementsByClassName("card-header")[0].innerHTML = curName;
+    let curPayer = payerList.get(numPayers);
+    let curName = curPayer.name;
+    let curCard = document.body.lastElementChild;
+    curCard.getElementsByClassName("card-header")[0].innerHTML = curName + `<button id="${numPayers}" type="button" class="btn btn-secondary modal-button" data-toggle="modal" data-target="#edit-contributor-modal">Edit</button>`;
     curCard.getElementsByClassName("card-title")[0].innerHTML = " Contributed:";
     curCard.getElementsByClassName("card-text")[0].innerHTML = "$" + payerContribution;
+
+    tagEditBtns()
 }
 
 
 
+function populateModal(event) {
+    let curPayer = payerList.get(parseInt(event.target.id));
+    let curName = curPayer.name;
+    let curContribution = curPayer.amountPaid;
+    let modalName = document.getElementById("editPayerName");
+    let modalContribution = document.getElementById("editPayerContribution");
+    modalName.value = curName;
+    modalContribution.value = curContribution;
+}
 
-
-
-
+function tagEditBtns() {
+    //add event listeners to edit btns in cards
+    var modalButtons = document.getElementsByClassName("modal-button");
+    Array.from(modalButtons).forEach(function(element) {
+        element.addEventListener('click', populateModal);
+        //console.log(modalButtons);
+    });
+}
 
 class BillPayer {
     constructor(name, amountPaid, amountOwed) {
